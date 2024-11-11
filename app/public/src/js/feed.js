3710832709ca22571df5b3f36d937a056763806f
Georgi Ivanov <georgi.ivanov85@hotmail.com>
2024-11-11 14:54:00 +0100

    add dynamic cache size limit

--- app/public/src/js/feed.js
+++ app/public/src/js/feed.js
@@ -78,7 +78,16 @@ function createCard() {
 var url = 'https://httpbin.org/get';
 var networkDataReceived = false;
 
-fetch(url)
+fetch(url, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  },
+  body: JSON.stringify({
+    message: 'Some message'
+  })
+})
   .then(function(res) {
     return res.json();
   })
